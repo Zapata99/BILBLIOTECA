9808562799883ea9fe98591898ff6f91ad23e9cb
Zapata99 <83059356+Zapata99@users.noreply.github.com>
2021-05-18 17:00:30 +0200

    esto asi
    hola

--- src/ABAJAO.java
+++ src/ABAJAO.java
@@ -5,7 +5,7 @@ public class ABAJAO {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner ("System.in");
-		System.out.println("Hola fiera");
+		System.out.println("Hola fiera que pasa mkina");
 	}
 
 }
